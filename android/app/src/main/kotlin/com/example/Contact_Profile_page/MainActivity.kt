371d6bff370968691c70fe909312ae217f21d91b
Madhuri <madhuriteli07@gmail.com>
2020-11-25 19:52:00 +0530

    stateless Contact page create and commited

---
+++ android/app/src/main/kotlin/com/example/Contact_Profile_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Contact_Profile_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,255 @@
+//contact profile page use with .dart extension
+
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(ContactProfilePage());
+
+class ContactProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.star_border),
+              color: Colors.black,
+              onPressed: () {
+                print("Contact is starred");
+              },
+            ),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  height: 250,
+                  child: Image.network(
+                    "https://cdn.pixabay.com/photo/2020/11/06/05/33/woman-5716875_1280.png",
+                    height: 250,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Ms. ABS",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 8, bottom: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      //Adding Profile action items
+                      buildCallButton(),
+                      buildTextButton(),
+                      buildVideoCallButton(),
+                      buildEmailButton(),
+                      buildDirectionsButton(),
+                      buildPayButton(),
+                    ],
+                  ),
+                ),
+                //Divider to separate action items section
+                Divider(
+                  color: Colors.grey,
+                ),
+
+                //Adding Phone numbers items
+                mobilePhoneListTile(),
+                otherPhoneListTile(),
+
+                //Divider to separate out phone numbers section
+                Divider(
+                  color: Colors.grey,
+                ),
+
+                //NEW CODE: Email address item
+                emailListTile(),
+
+                //NEW CODE: Divider to separate physical address
+                Divider(
+                  color: Colors.grey,
+                ),
+
+                //NEW CODE: Contact's home address item
+                addressListTile(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //Adding "Call" action item
+  Widget buildCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.call,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Call"),
+      ],
+    );
+  }
+
+  //Adding "Text" action item
+  Widget buildTextButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.message,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Text"),
+      ],
+    );
+  }
+
+  //Adding "Video" action item
+  Widget buildVideoCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.video_call,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Video"),
+      ],
+    );
+  }
+
+  //Adding "Email" action item
+  Widget buildEmailButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.email,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Email"),
+      ],
+    );
+  }
+
+  //Adding "Directions" action item
+  Widget buildDirectionsButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.directions,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Directions"),
+      ],
+    );
+  }
+
+  //Adding "Pay" action item
+  Widget buildPayButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.attach_money,
+            color: Colors.indigo.shade800,
+          ),
+          onPressed: () {},
+        ),
+        Text("Pay"),
+      ],
+    );
+  }
+
+  //Adding "Mobile Phone Number" item
+  Widget mobilePhoneListTile() {
+    return ListTile(
+      leading: Icon(Icons.call),
+      title: Text("330-803-3390"),
+      subtitle: Text("mobile"),
+      trailing: IconButton(
+        icon: Icon(Icons.message),
+        color: Colors.indigo.shade500,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  //Adding "Other Phone Number" item
+  Widget otherPhoneListTile() {
+    return ListTile(
+      leading: Text(""),
+      title: Text("440-440-3390"),
+      subtitle: Text("other"),
+      trailing: IconButton(
+        icon: Icon(Icons.message),
+        color: Colors.indigo.shade500,
+        onPressed: () {},
+      ),
+    );
+  }
+
+  //NEW CODE: Adding "Email Address" item
+  Widget emailListTile() {
+    return ListTile(
+      leading: Icon(Icons.email),
+      title: Text("abc@.com"),
+      subtitle: Text("work"),
+    );
+  }
+
+  //NEW CODE: Adding Contact's home address
+  Widget addressListTile() {
+    return ListTile(
+      leading: Icon(Icons.location_on),
+      title: Text("abs home address"),
+      subtitle: Text("home"),
+      trailing: IconButton(
+        icon: Icon(Icons.directions),
+        color: Colors.indigo.shade500,
+        onPressed: () {},
+      ),
+    );
+  }
+}
